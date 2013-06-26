601e9376de119c04ef46cd4b2c690c71ccb2a725
Spencer Alderman <spam.mehere@hotmail.com>
2013-06-26 14:56:20 -0400

    Added saving to the config

--- src/main/java/com/rogue/regexblock/regex/RegexManager.java
+++ src/main/java/com/rogue/regexblock/regex/RegexManager.java
@@ -31,17 +31,17 @@ import org.bukkit.configuration.file.FileConfiguration;
  * @version 1.0
  */
 public class RegexManager {
-    
+
     private RegexBlock plugin;
     private Map<String, BlockRegex> regexes = new HashMap();
-    
+
     /**
      * Creates a new RegexManager, then creates and adds a BlockRegex object
      * from the provided data
-     * 
+     *
      * @since 1.0
      * @version 1.0
-     * 
+     *
      * @param p The RegexBlock plugin instance
      * @param name Name of the BlockRegex
      * @param reason The denial reason for when the regex is triggered
@@ -51,13 +51,13 @@ public class RegexManager {
         plugin = p;
         regexes.put(name, new BlockRegex(name, reason, regex));
     }
-    
+
     /**
      * Creates a new RegexManager, and adds any provided BlockRegex objects
-     * 
+     *
      * @since 1.0
      * @version 1.0
-     * 
+     *
      * @param p The RegexBlock plugin instance
      * @param regex Any BlockRegex objects
      */
@@ -67,83 +67,86 @@ public class RegexManager {
             regexes.put(temp.getName(), temp);
         }
     }
-    
+
     /**
      * Adds provided BlockRegex(es) to the regex Map
-     * 
+     *
      * @since 1.0
      * @version 1.0
-     * 
+     *
      * @param regex Any BlockRegex objects
-     * 
+     *
      * @return The updated map of regexes
      */
     public Map<String, BlockRegex> addRegex(BlockRegex... regex) {
         for (BlockRegex temp : regex) {
             regexes.put(temp.getName(), temp);
+            addToFile(temp.getName());
         }
         return regexes;
     }
-    
+
     /**
      * Creates a new BlockRegex object and adds it to the current regexes
-     * 
+     *
      * @since 1.0
      * @version 1.0
-     * 
+     *
      * @param name Name of the BlockRegex
      * @param reason The denial reason for when the regex is triggered
      * @param regex The pattern to match against for the regex
-     * 
+     *
      * @return The updated map of regexes
      */
     public Map<String, BlockRegex> addRegex(String name, String reason, String regex) {
         regexes.put(name, new BlockRegex(name, reason, regex));
+        addToFile(name);
         return regexes;
     }
-    
-    /** 
+
+    /**
      * Returns the full Map of all the regexes
-     * 
+     *
      * @since 1.0
      * @version 1.0
-     * 
+     *
      * @return The Map of regexes
      */
     public Map<String, BlockRegex> getRegexes() {
         return regexes;
     }
-    
+
     /**
      * Gets a BlockRegex by name
-     * 
+     *
      * @since 1.0
      * @version 1.0
-     * 
+     *
      * @param name The name of the BlockRegex
      * @return The BlockRegex, null if provided name does not exist
      */
     public BlockRegex getRegex(String name) {
         return regexes.get(name);
     }
-    
+
     /**
      * Removes a BlockRegex by its designated name
-     * 
+     *
      * @since 1.0
      * @version 1.0
-     * 
+     *
      * @param name The regex's name
      * @return The updated map of regexes
      */
     public Map<String, BlockRegex> remRegex(String name) {
         regexes.remove(name);
+        remFromFile(name);
         return regexes;
     }
-    
+
     /**
      * Loads any possible regex configurations from the config file
-     * 
+     *
      * @since 1.0
      * @version 1.0
      */
@@ -153,11 +156,50 @@ public class RegexManager {
         Set<String> allregexes = stuff.getKeys(false);
         for (String name : allregexes) {
             if ((!regexFile.getString("regexes." + name + ".reason").equals("") && regexFile.getString("regexes." + name + ".reason") != null)
-               && (!regexFile.getString("regexes." + name + ".regex").equals("") && regexFile.getString("regexes." + name + ".regex") != null)) {
-             this.addRegex(name, regexFile.getString("regexes." + name + ".reason"), regexFile.getString("regexes." + name + ".regex"));
-         } else {
-             plugin.getLogger().log(Level.SEVERE, "Bad regex in config: ''{0}''!", name);
-         }
+                    && (!regexFile.getString("regexes." + name + ".regex").equals("") && regexFile.getString("regexes." + name + ".regex") != null)) {
+                this.addRegex(name, regexFile.getString("regexes." + name + ".reason"), regexFile.getString("regexes." + name + ".regex"));
+            } else {
+                plugin.getLogger().log(Level.SEVERE, "Bad regex in config: ''{0}''!", name);
+            }
         }
+    }
+    
+    /**
+     * Adds a BlockRegex to the plugin's config, then saves it
+     * 
+     * @since 1.0
+     * @version 1.0
+     * 
+     * @param name The name of the regex
+     */
+    public void addToFile(String name) {
+        FileConfiguration regexFile = plugin.getConfig();
+        regexFile.set("regexes." + name + "regex", regexes.get(name).getPattern().toString());
+        regexFile.set("regexes." + name + "reason", regexes.get(name).getReason());
+        saveFile();
+    }
+    
+    /**
+     * Removes a BlockRegex from the plugin's config, then saves it
+     * 
+     * @since 1.0
+     * @version 1.0
+     * 
+     * @param name The name of the regex
+     */
+    public void remFromFile(String name) {
+        FileConfiguration regexFile = plugin.getConfig();
+        regexFile.set("regexes." + name, null);
+        saveFile();
+    }
+    
+    /**
+     * Saves the currently loaded configuration
+     * 
+     * @since 1.0
+     * @version 1.0
+     */
+    public void saveFile() {
+        FileConfiguration regexFile = plugin.getConfig();
     }
 }
